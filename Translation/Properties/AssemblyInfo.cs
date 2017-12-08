1df63549aa7e5e9d6006832d0c77f4017ad75519
蓝贵才 <@LAN-PC>
2017-12-08 23:31:00 +0800

    更改程序集

--- Translation/Properties/AssemblyInfo.cs
+++ Translation/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Translation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("一款接入谷歌翻译的翻译软件")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Lan")]
 [assembly: AssemblyProduct("Translation")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Lan 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
